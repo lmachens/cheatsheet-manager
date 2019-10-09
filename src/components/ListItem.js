effd9ff8ae0bb5ab9197f260524d964927e576fe
lmachens <leon.machens@googlemail.com>
2019-10-09 11:38:53 +0200

    add ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,65 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import More from "../icons/More";
+import Less from "../icons/Less";
 
+function getColor(props) {
+  if (props.active && props.indent) {
+    return "#09d3ac";
+  }
+  if (props.active && !props.indent) {
+    return "#09d3ac";
+  }
+  if (!props.active && props.indent) {
+    return "#b7b7b7";
+  }
+  if (!props.active && !props.indent) {
+    return "#fff";
+  }
+}
 const StyledLi = styled.li`
   list-style: none;
   font-weight: bold;
+  color: ${getColor};
+  fill: ${getColor};
+  cursor: pointer;
+  margin-left: ${props => props.indent && "20px"};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &:hover {
+    color: #09d3ac;
+  }
 `;
 
-export default function ListItem() {
-  return <StyledLi>ListItem</StyledLi>;
+export default function ListItem({
+  active,
+  children,
+  details,
+  indent,
+  onClick
+}) {
+  const showMore = details && !active;
+  const showLess = details && active;
+
+  return (
+    <>
+      <StyledLi active={active} indent={indent} onClick={onClick}>
+        {children}
+        {showMore && <More />}
+        {showLess && <Less />}
+      </StyledLi>
+      {active && details}
+    </>
+  );
 }
+
+ListItem.propTypes = {
+  active: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  indent: PropTypes.bool,
+  details: PropTypes.node,
+  onClick: PropTypes.func
+};
--- src/stories/Lists.stories.js
+++ src/stories/Lists.stories.js
@@ -5,5 +5,45 @@ export default {
   title: "Lists"
 };
 
-export const TopLevelItem = () => <ListItem />;
-export const SubLevelItem = () => <ListItem />;
+export const TopLevelItem = () => <ListItem>HTML</ListItem>;
+export const ActiveTopLevelItem = () => <ListItem active>CSS</ListItem>;
+export const SubLevelItem = () => <ListItem indent>Semantic</ListItem>;
+export const ActiveSubLevelItem = () => (
+  <ListItem active indent>
+    Positions
+  </ListItem>
+);
+
+export const ExampleList = () => {
+  const [active, setActive] = React.useState("html");
+  return (
+    <>
+      <ListItem
+        details={
+          <>
+            <ListItem indent>Semantic</ListItem>
+            <ListItem indent>Header</ListItem>
+            <ListItem indent>Inline</ListItem>
+            <ListItem indent>Block</ListItem>
+          </>
+        }
+        onClick={() => setActive("html")}
+        active={active === "html"}
+      >
+        HTML
+      </ListItem>
+
+      <ListItem
+        active={active === "css"}
+        onClick={() => setActive("css")}
+        details={
+          <>
+            <ListItem indent>Positions</ListItem>
+          </>
+        }
+      >
+        CSS
+      </ListItem>
+    </>
+  );
+};
